a6601d70ece1ee5a2c4567a3551cbd6a3fb877f1
b-rajshekar <brajashekhar19@gmail.com>
2023-01-23 06:54:58 +0530

    Added: Javascript

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-let userName = prompt("What is you name");
-alert(userName);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#text-input");
+
+console.log(txtInput);
+
+// btnTranslate.addEventListener("click", function clickEventHandler() {
+//     console.log("clicked");
+// });
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
